286f54f9f4f5e488574543bbecbadffad3b918f3
olaide-hok <olaidekareem43@yahoo.com>
2022-06-19 18:46:59 +0100

    Rename project

---
+++ olaide_hok/olaide_hok/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for I4G007229ILN project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'olaide_hok.settings')
+
+application = get_wsgi_application()
